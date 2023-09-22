d2fa9726e7ae442ad47bea20b229806397d4a3c7
Denis <denis_malikov_01@mail.ru>
2023-09-22 22:44:42 +0500

    oop7

---
+++ ComplexFactory.java
@@ -0,0 +1,6 @@
+abstract class ComplexFactory {
+    abstract Complex add(Complex other);
+    abstract Complex subtract(Complex other);
+    abstract Complex multiply(Complex other);
+    abstract Complex divide(Complex other);
+}
